a8023b536367eeddae36aa31dd743d953d8a0368
Kevin <kevintun1122@gmail.com>
2018-07-06 16:18:48 -0700

    - Set up loader.io

--- server/index.js
+++ server/index.js
@@ -21,9 +21,7 @@ App.use(compress());
 App.use(express.static(path.join(__dirname, '../client/dist/')));
 
 App.use('/api', router)
-App.get('/loaderio-78f8b3520df2d87fd07c3b704c5d3588.txt', (req, res) => {
-  res.sendFile(path.join(__dirname, '../loaderIO/loaderio-78f8b3520df2d87fd07c3b704c5d3588.txt'));
-})
+
 // console.log(App.get('env'));
 App.listen(PORT, err => {
     err ? console.log('Failed to start server: ', err) : console.log('Listening on port ', PORT)
